a3eb5b3c7c399d1d18c18bb6dc4ede19b3ba8b37
p.bhatra@thefifthelement.co <bhatraprerna061@gmail.com>
2023-08-19 11:50:04 +0530

    read and listen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,99 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function TabOneScreen() {
+
+  const [data, setData] = useState(null);
+  const [purans, setPurans] = useState([])
+
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Make the API call when the component mounts
+    fetchData();
+  }, []);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://doubtful-pear-squid.cyclic.cloud/puran/');
+      // console.log({ response });
+
+      const jsonData = await response.json();
+      // console.log({ jsonData });
+
+      setPurans(jsonData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+  const handleItemPress = (_id: any) => {
+    // Navigate to a new screen and pass the _id as a parameter
+    navigation.navigate('two', { itemId: _id })
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text >Purans</Text>
+      <ScrollView >
+      {purans.length > 0 && purans.map((item: any) => (
+     
+        <TouchableOpacity
+          key={item._id}
+          style={styles.itemContainer}
+          onPress={() => handleItemPress(item._id)}
+        >
+          <View style={styles.itemContainer} >
+            <Text style={styles.name}>{item.name}</Text>
+            <Image source={{ uri: item.image }} style={{ width: 100, height: 100 }} />
+            <Text style={styles.description}>{item.description}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+              </ScrollView>
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
   },
-  title: {
-    fontSize: 20,
+  itemContainer: {
+    marginBottom: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  name: {
+    color: 'black',
+    fontSize: 18,
     fontWeight: 'bold',
+    marginTop: 8,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  description: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 4,
   },
 });
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,186 @@
-import { StyleSheet } from 'react-native';
+import { Button, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useRoute } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
+import * as Speech from 'expo-speech'; // Import the Speech module
+
+
 
 export default function TabTwoScreen() {
+  const route = useRoute();
+  const { itemId } = route.params; // Access the itemId from route.params
+
+  const [itemData, setItemData] = useState([]);
+
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const toggleChapter = (index: any) => {
+    if (index === expandedIndex) {
+      setExpandedIndex(-1); // Collapse the clicked chapter
+    } else {
+      setExpandedIndex(index); // Expand the clicked chapter
+    }
+  };
+
+  useEffect(() => {
+    // Fetch data using the itemId
+
+    // Tts.setDefaultLanguage('hi-IN')
+    fetchData(itemId);
+  }, [itemId]);
+
+
+
+  const fetchData = async (id: any) => {
+    try {
+      const response = await fetch('https://doubtful-pear-squid.cyclic.cloud/puran/chapterByPuran', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          puranId: itemId
+        }),
+      });
+
+      const jsonResponse = await response.json();
+
+      setItemData(jsonResponse.data)
+    } catch (error) {
+      console.error('Error making POST request:', error);
+    }
+  };
+
+
+  const handleListen = (content: string) => {
+    const chunkSize = 2000; // Set the desired chunk size (adjust as needed)
+    const chunks = [];
+
+    for (let i = 0; i < content.length; i += chunkSize) {
+      chunks.push(content.slice(i, i + chunkSize));
+    }
+
+    playChunksSequentially(chunks);
+  };
+
+
+  const playChunksSequentially = async (chunks: Array<string>) => {
+    for (const chunk of chunks) {
+      try {
+        await Speech.speak(chunk, {
+          language: 'hi-IN', // Set the language code for Hindi (India)
+          rate: 0.8,
+        });
+      } catch (error) {
+        console.error("Error while playing chunk:", error);
+      }
+    }
+  };
+
+  const handlePause = () => {
+    Speech.pause();
+  };
+
+  const handleStop = () => {
+    Speech.stop();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <ScrollView >
+        <Text style={styles.title} >Chapters</Text>
+        {itemData && itemData?.length > 0 && itemData.map((item: any, index) => (
+          <View key={index} style={styles.chapterContainer}>
+            <TouchableOpacity onPress={() => toggleChapter(index)}>
+              <Text style={styles.chapterTitle}>Chapter {index + 1}</Text>
+              <Text style={styles.itemName}>{item.title}
+              </Text>
+
+            </TouchableOpacity>
+            {expandedIndex === index && (
+              <>
+                <ScrollView style={styles.itemContentContainer}>
+                  {/* <FontAwesomeIcon icon={faHeadphones} style={styles.listenIcon} /> */}
+                  <View style={styles.listenButtonContainer}>
+                    <Button
+                      title="Listen"
+                      onPress={() => handleListen(item.content)}
+                    />
+                    <View style={styles.listenButtonSpacer} />
+                    <Button
+                      title="Pause"
+                      onPress={handlePause}
+                    />
+                    <View style={styles.listenButtonSpacer} />
+
+                    <Button
+                      title="Stop"
+                      onPress={handleStop}
+                    />
+                  </View>
+                  <Text >{item.content}</Text>
+                </ScrollView>
+              </>
+            )}
+          </View>
+        ))}
+      </ScrollView>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: 'black',
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 16,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  chapterContainer: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 16, // Add margin between chapters
+  },
+  chapterTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  itemContentContainer: {
+
+  },
+  listenIcon: {
+    borderColor: 'pink',
+    color: 'white',
+    fontSize: 16
+  },
+  // listenIcon: {
+  //   marginLeft: 8, // Adjust the spacing between text and icon
+  //   fontSize: 16, // Adjust the icon size
+  //   color: '#888', // Adjust the icon color
+  // },
+  listenButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  listenButtonSpacer: {
+    width: 10,
+  },
+  itemName: {
+    fontSize: 16,
+    marginTop: 4,
   },
 });
